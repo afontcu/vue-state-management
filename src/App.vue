02c36a56855b08bf1a5f8c1037e3116fbd0ae88d
Adrià Fontcuberta <adria@calidae.com>
2018-07-30 18:14:17 +0200

    Step 2 - Shared object

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,26 @@
     <button @click="voteForBlue">Vote for 🔵</button>
 
     <h2>Results</h2>
-    <results :red="red" :blue="blue" />
-    <total-votes :total="red + blue" />
+    <results/>
+    <total-votes/>
   </div>
 </template>
 
 <script>
+const state = {
+  red: 0,
+  blue: 0,
+}
+
 const TotalVotes = {
-  props: ['total'],
+  data () { return state },
   render (h) {
-    return h('div', `Total votes: ${this.total}`)
+    return h('div', `Total votes: ${this.red + this.blue}`)
   },
 }
 
 const Results = {
-  props: ['red', 'blue'],
+  data () { return state },
   render (h) {
     return h('div', `Red: ${this.red} - Blue: ${this.blue}`)
   },
@@ -27,7 +32,7 @@ const Results = {
 
 export default {
   components: { TotalVotes, Results },
-  data: () => ({ red: 0, blue: 0 }),
+  data () { return state },
   methods: {
     voteForRed () { this.red++ },
     voteForBlue () { this.blue++ },
